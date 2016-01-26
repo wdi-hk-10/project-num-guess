a6b5c13cc17a4e2b64dbf970e7e5e09fca615a6d
Julien Waite <julienwaite@gmail.com>
2016-01-26 14:30:39 +0800

    HTML & CSS continued.

--- js/application.js
+++ js/application.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){ // do not remove - insert all code in here!
 
+  //event listener
+  //$('#game-screen').hide()
+  //$('#instruct-screen').show()
+
+  //event listener
+  //$('#options').hide()
+  //$('#play-box').show()
+
+
+  //var threeX3 = [[1],[2],[3],[4].[5],[6],[7],[8],[9]];
+  //var fourX4 = [[1],[2],[3],[4].[5],[6],[7],[8],[9]];
+  //var fiveX5 = [[1],[2],[3],[4].[5],[6],[7],[8],[9]];
 }); // do not remove
